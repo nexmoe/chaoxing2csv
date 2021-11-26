9fc5905fc3892ead0214859a17cac7a8a5b8a167
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-11-26 18:17:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,9 @@
 // @version      0.1.1
 // @description  将你的超星学习通里的作业数据、随堂练习导出成为 Csv 文件，方便导入 Anki 背题
 // @author       Nexmoe
+// @github       https://github.com/nexmoe/chaoxing2csv
 // @namespace     https://nexmoe.com/
 // @match        *://*.chaoxing.com/*
-// @grant        GM_setValue
-// @grant        GM_getValue
 // @license      MIT
 // ==/UserScript==
 
