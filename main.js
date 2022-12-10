b69ef4c89702253e5db43bfd551af0d5959faba6
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2022-12-10 19:43:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
 // ==UserScript==
 // @name         超星 To Csv
-// @version      0.1.4
+// @version      0.2.0
 // @description  将你的超星学习通里的作业数据、随堂练习导出成为 Csv 文件，方便导入 Anki 背题
 // @author       Nexmoe
 // @github       https://github.com/nexmoe/chaoxing2csv
-// @namespace     https://nexmoe.com/
+// @namespace    https://nexmoe.com/
 // @match        *://*.chaoxing.com/*
 // @license      MIT
 // ==/UserScript==
 
-// 在此处键入代码……
-let css = () => {
-    let style = document.createElement("style");
-    style.type = "text/css";
-    let text = document.createTextNode("textarea { border: 1px solid; width: 100%; resize: none;}");
-    style.appendChild(text);
-    let head = document.getElementsByTagName("head")[0];
-    head.appendChild(style);
+const getPage = () => {
+    if (document.querySelector(".detailsHead")) {
+        return "homework";
+    } else if (document.querySelector(".top-box")) {
+        return "practice";
+    } else {
+        return "invalid";
+    }
 }
 
-let generateTextArea = (data) => {
+const page = getPage();
+console.log(page)
+
+const generateTextArea = (data) => {
     let node = document.createElement("textarea");
+    node.style = `border: 3px solid;
+        width: 100%;
+        resize: none;
+        box-sizing: border-box;
+        height: 70px;`;
     let textnode = document.createTextNode(data);
     node.appendChild(textnode);
-    if (document.getElementsByClassName("detailsHead").length > 0) {
+    if (page == "homework") {
         document.getElementsByClassName("detailsHead")[0].appendChild(node);
     } else {
         document.getElementsByClassName("top-box")[0].appendChild(node);
@@ -52,17 +60,14 @@ let saveFile = (data) => {
     const blob = new Blob([data], { type: 'text/csv;charset=utf-8;' });
     const Url = URL.createObjectURL(blob);
     let link = document.createElement('a');
-    if (document.getElementsByClassName("item-name").length > 0) {
-        link.download = document.getElementsByClassName("item-name")[0].innerText + '.csv';	//文件名字
-    } else {
-        link.download = document.getElementsByClassName("mark_title")[0].innerText + '.csv';	//文件名字
-    }
-
     link.href = Url;
-    link.appendChild(document.createTextNode('下载题目数据'));
-    if (document.getElementsByClassName("detailsHead").length > 0) {
+    link.appendChild(document.createTextNode('下载题目 CSV 数据'));
+
+    if (page == "homework") {
+        link.download = document.querySelector(".mark_title").innerText + '.csv';	//文件名字
         document.getElementsByClassName("detailsHead")[0].appendChild(link);
     } else {
+        link.download = document.querySelector(".textHeightauto").innerHTML + '.csv';	//文件名字
         document.getElementsByClassName("top-box")[0].appendChild(link);
     }
 }
@@ -85,99 +90,159 @@ let optionsFilter = (dom, i = 0, strings = "") => {
 }
 
 let answersFilter = (dom, i = 0, strings = "") => {
-    answer = dom.innerText.replace("正确答案: ", "").replace(
-        new RegExp(",", "gm"),
-        "，"
-    ).replace(
-        new RegExp(" ", "gm"),
-        ""
-    ).replace(
-        new RegExp("对", "gm"),
-        "A"
-    ).replace(
-        new RegExp("错", "gm"),
-        "B"
-    );
+    try {
+        answer = dom.innerText.replace("正确答案: ", "").replace(
+            new RegExp(",", "gm"),
+            "，"
+        ).replace(
+            new RegExp(" ", "gm"),
+            ""
+        ).replace(
+            new RegExp("对", "gm"),
+            "A"
+        ).replace(
+            new RegExp("错", "gm"),
+            "B"
+        );
 
-    for (i = 0; i < answer.length; i++) {
-        if (i < (answer.length - 1)) {
-            strings = strings + (answer.charCodeAt(i) - 64) + "||"
-        } else {
-            strings = strings + (answer.charCodeAt(i) - 64)
+        for (i = 0; i < answer.length; i++) {
+            if (i < (answer.length - 1)) {
+                strings = strings + (answer.charCodeAt(i) - 64) + "||"
+            } else {
+                strings = strings + (answer.charCodeAt(i) - 64)
+            }
+
         }
-
+    } catch (e) {
+        strings = dom.innerHTML;
+        console.error(e)
     }
 
     return strings;
 }
 
 let questionsFilter = (dom, answers, i = 0, strings = "") => {
-    question = dom.innerHTML.replace(
-        new RegExp(",", "gm"),
-        "，"
-    ).replace(
-        new RegExp("\n", "gm"),
-        ""
-    ).replace(
-        new RegExp("http://", "gm"),
-        "https://"
-    );
-    answer = answers.innerText.replace("正确答案: ", "").replace(
-        new RegExp(",", "gm"),
-        "，"
-    ).replace(
-        new RegExp(" ", "gm"),
-        ""
-    ).replace(
-        new RegExp("对", "gm"),
-        "A"
-    ).replace(
-        new RegExp("错", "gm"),
-        "B"
-    );
+    let question;
+    try {
+        question = dom.innerHTML.replace(
+            new RegExp(",", "gm"),
+            "，"
+        ).replace(
+            new RegExp("\n", "gm"),
+            ""
+        ).replace(
+            new RegExp("http://", "gm"),
+            "https://"
+        );
+        answer = answers.innerText.replace("正确答案: ", "").replace(
+            new RegExp(",", "gm"),
+            "，"
+        ).replace(
+            new RegExp(" ", "gm"),
+            ""
+        ).replace(
+            new RegExp("对", "gm"),
+            "A"
+        ).replace(
+            new RegExp("错", "gm"),
+            "B"
+        );
 
-    for (i = 0; i < answer.length; i++) {
-        strings = strings + "{{c" + (i+ 1) + "::}}"
-
+        for (i = 0; i < answer.split('\n').length; i++) {
+            strings = strings + "{{c" + (i + 1) + "::}}"
+        }
+    } catch (e) {
+        console.error(e)
     }
 
     return question + strings;
 }
 
+const domKey = {
+    typeList: [
+        "(单选题)",
+        "(多选题)",
+        "(判断题)",
+    ],
+    typeToKey: {
+        "(单选题)": "select",
+        "(多选题)": "select",
+        "(判断题)": "select",
+    },
+    homework: {
+        type: ".mark_name .colorShallow",
+        select: {
+            question: ".mark_name",
+            options: ".mark_letter",
+            options2: ".stem_answer",
+            answer: ".colorGreen",
+            answer2: ".check_answer"
+        },
+    },
+    practice: {
+        type: ".topic-title .topic-type",
+        select: {
+            question: ".topic-title",
+            options: ".topic-options",
+            answer: ".color-green"
+        },
+    }
+}
+
 let getData = (i, data = []) => {
-    let questions, selections, answers, notes;
-    if (document.getElementsByClassName("mark_name").length > 0) {
-        questions = document.getElementsByClassName("mark_name");
-        selections = document.getElementsByClassName("mark_letter");
-        answers = document.getElementsByClassName("colorGreen");
-    } else {
-        questions = document.getElementsByClassName("topic-title");
-        selections = document.getElementsByClassName("topic-options");
-        answers = document.getElementsByClassName("color-green");
+    let questionList = page == "homework" ? document.querySelectorAll(".questionLi") : document.querySelectorAll(".question-list");
 
-    }
-
-    for (i = 0; i < questions.length; i++) {
-        data.push({
+    for (let child of questionList) {
+        let type, question, options, answer, questItem;
+        type = child.querySelector(domKey[page].type).innerText.replace(" ", "");
+        if (!domKey.typeList.includes(type)) {
+            console.log("不支持：", type)
+            continue;
+        } else {
+            type = domKey.typeToKey[type];
+        }
+        const qDom = child.querySelector(domKey[page][type].question);
+        const oDom = child.querySelector(domKey[page][type].options) || child.querySelector(domKey[page][type].options2);
+        const aDom = child.querySelector(domKey[page][type].answer) || child.querySelector(domKey[page][type].answer2);
+        try {
+            question = questionsFilter(qDom, aDom);
+        } catch (e) {
+            console.error(e)
+        }
+        try {
+            options = optionsFilter(oDom);
+        } catch (e) {
+            console.error(e)
+        }
+        try {
+            answer = answersFilter(aDom);
+        } catch (e) {
+            console.error(e)
+        }
+        questItem = {
+            question,
+            options,
+            answer,
             id: String(new Date().getTime()) + String(Math.floor(Math.random() * 100000)),
-            question: questionsFilter(questions[i], answers[i]),
-            options: optionsFilter(selections[i]),
-            answer: answersFilter(answers[i]),
-        })
+            type,
+        }
+        console.log(questItem)
+        data.push(questItem)
     }
-    console.log(data)
+
     return data;
 }
 
-(function () {
-    css();
-    if (document.getElementsByClassName("mark_name").length > 0) {
-        generateTextArea(JSON.stringify(getData()));
-        saveFile(ConvertToCSV(JSON.stringify(getData())));
-    } else {
-        setTimeout(() => {
-            generateTextArea(JSON.stringify(getData()));
-            saveFile(ConvertToCSV(JSON.stringify(getData())));
-        }, 1000)
+(() => {
+    for (let child of document.querySelectorAll(".questionLi *")) {
+        child.style = "";
+        delete child.style;
     }
+    setTimeout(() => {
+        if (page !== "invalid") {
+            const data = JSON.stringify(getData());
+            generateTextArea(ConvertToCSV(data));
+            saveFile(ConvertToCSV(data));
+        }
+    }, 1000)
 })();
